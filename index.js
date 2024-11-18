5f289d7db4573603c9941bc6ae53560f2fdc182a
OLspectre <ossi9910@gmail.com>
2024-11-18 17:15:19 +0100

    textContent added to existing city div works but is also only temporary. Correct distance (variable) will be added later
    Renamed variable tableDiv to cityListDiv to not get confused about the actual table further down the page later.

--- index.js
+++ index.js
@@ -6,12 +6,12 @@
 
 const fakePrompt = "malmö"
 
-let tableDiv = document.getElementById("cities");
+const cityListDiv = document.getElementById("cities");
 
 function createAllCityBoxes() {
     for (let city of cities) {
         let eachCityName = document.createElement("div");
-        tableDiv.appendChild(eachCityName);
+        cityListDiv.appendChild(eachCityName);
         eachCityName.className = "cityBox";
         eachCityName.textContent = `${city.name}`;
         eachCityName.id = city.name; // unikt id med stadens namn, object city med värdet av nyckeln name
@@ -26,13 +26,13 @@ alltså argumentet som är ett objekt och objektets nyckel (name) = namnet på s
         promptObject.classList.add("target");
     } else if (kindOfCity == "closest") {
         promptObject.classList.add("closest");
-        // TextContent som adderar string += `ligger ${variabel av avstånd} mil bort`
+        promptObject.textContent += ` ligger ${cityObject.name} mil bort` // temporär variabel till senare operation för avstånd.
     } else if (kindOfCity == "furthest") {
         promptObject.classList.add("furthest");
-        // TextContent som adderar string += `ligger ${variabel av avstånd} mil bort`
+        promptObject.textContent += ` ligger ${cityObject.name} mil bort`
     }
     console.log(cityObject.name) // Test om det fungerar
 };
 
 markCityObject(cities[2], "furthest")
-console.log(tableDiv)
+console.log(cityListDiv)
