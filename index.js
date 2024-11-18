c5b0231b781bb826073f03fdb97833960f71a32f
OLspectre <ossi9910@gmail.com>
2024-11-18 14:06:54 +0100

    Programmed "createAllCityBoxes function with a for of loop inside creating a div for each city. - Tested and works.
    city.name selecting the key "name" of the object "city".

--- index.js
+++ index.js
@@ -4,3 +4,18 @@
 
 // Recommended: Ask for the city name and then the rest of the code
 
+
+let tableDiv = document.getElementById("cities");
+
+function createAllCityBoxes() {
+    for (let city of cities) {
+        let eachCityName = document.createElement("div");
+        tableDiv.appendChild(eachCityName);
+        eachCityName.className = "cityBox";
+        eachCityName.textContent = `${city.name}`
+
+    }
+}
+createAllCityBoxes();
+
+console.log(tableDiv)
