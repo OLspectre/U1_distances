dc3857ab9aae3898792d7d18d31fb061b21d7c75
OLspectre <ossi9910@gmail.com>
2024-11-18 16:11:22 +0100

    Declared function markCityObject to target correct city (div) using unique ID depending on what is set as argument.
    Arguments are now hard coded to check if it targets the right city div.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 // Recommended: Ask for the city name and then the rest of the code
 
+const fakePrompt = "malmö"
 
 let tableDiv = document.getElementById("cities");
 
@@ -12,10 +13,18 @@ function createAllCityBoxes() {
         let eachCityName = document.createElement("div");
         tableDiv.appendChild(eachCityName);
         eachCityName.className = "cityBox";
-        eachCityName.textContent = `${city.name}`
-
+        eachCityName.textContent = `${city.name}`;
+        eachCityName.id = city.name; // unikt id med stadens namn, object city med värdet av nyckeln name
     }
-}
+};
 createAllCityBoxes();
 
+function markCityObject(cityObject, kindOfCity) {
+    let promptObject = document.getElementById(cityObject.name); /* promptObject refererar till ett HTML-element med ID (argumentet.name), 
+                                                                 alltså argumentet som är ett objekt och objektets nyckel (name) = namnet på staden */
+    console.log(cityObject.name) // Test om det fungerar
+    // Skapa If-satser för att ge unik styling beroende på kindOfCity "target" "closest" "furthest"
+};
+
+markCityObject(cities[2], "target")
 console.log(tableDiv)
